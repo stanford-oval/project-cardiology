6ed64297d6981a91f5252680fbbe5a386788bd3f
rickygv99 <rickygv99@yahoo.com>
2019-07-01 11:47:53 -0700

    Set up Twilio to send SMS messages

--- device.js
+++ device.js
@@ -1,16 +1,33 @@
 "use strict";
 
 const Tp = require('thingpedia');
+const twilio = require('twilio'); // https://www.twilio.com
+
+// These values can be found in the Twilio console.
+const TWILIO_ACCOUNT_SID = 'ACe43881bb16f358819e5e0150b7d30f2b';
+const TWILIO_AUTH_TOKEN = 'a231bfe257bb39db74731b7e354441da';
+var client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+/*
+ * Formats the ruleset so that it is readable by humans.
+ */
+function format_ruleset(ruleset) {
+  // TODO: Insert code here
+
+  return ruleset;
+}
 
 module.exports = class Cardiology extends Tp.BaseDevice {
   constructor(engine, state) {
     super(engine, state);
 
-    this.uniqueId = "me.cardiology-" + this.state.userId;
+    this.uniqueId = "cardiology-" + this.state.userId;
     this.name = "Cardiology Account for " + this.state.screenName;
-    this.description = "A virtual assistant, based on Almond, that helps doctors"
-      + " remind patients to track their blood pressure regularly."
-      + "This is the account for " + this.state.screenName;
+    this.description = "This is your Cardiology Account. You can use it to"
+      + " remind patients to track their blood pressure regularly.";
+
+    this._ruleset = null;
+    this._feed = null;
   }
 
   /*
@@ -36,10 +53,8 @@ module.exports = class Cardiology extends Tp.BaseDevice {
    * recently created
    */
   get_ruleset() {
-    ruleset = null;
-
-    // TODO: Insert code here
-
+    var ruleset = this._ruleset;
+    ruleset = format_ruleset(ruleset);
     return [{ ruleset: ruleset }];
   }
 
@@ -53,9 +68,18 @@ module.exports = class Cardiology extends Tp.BaseDevice {
 
   /*
    * Send the blood pressure measurement instructions to the
-   * patient's virtual assistant
+   * patient's virtual assistant.
+   * Each patient is uniquely identified by their phone number
    */
-  do_send_ruleset() {
-    // TODO: Insert code here
+  do_send_ruleset({ phone_number }) {
+    var ruleset = this._ruleset;
+    ruleset = format_ruleset(ruleset);
+
+    // Sends the SMS message through Twilio
+    client.messages.create({
+      to: phone_number,
+      from: '17149474157', // Twilio trial number
+      body: ruleset
+    });
   }
 };
