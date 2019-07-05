330c42935fa696d97aca5b231c4f5f25d571fbe6
rickygv99 <rickygv99@yahoo.com>
2019-07-05 14:14:50 -0700

    Created doctor and patient Thingpedia interfaces

--- doctortt/device.js
+++ doctortt/device.js
@@ -1,6 +1,18 @@
 "use strict";
 
 const Tp = require('thingpedia');
+const https = require('https');
+
+var options_retrieve = {
+  hostname: 'https://almond-cardiology.herokuapp.com',
+  path: '/retrieve?', // Query parameters are added later during function call
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Requested-With'
+  }
+};
 
 module.exports = class Cardiology_Doctor extends Tp.BaseDevice {
   constructor(engine, state) {
@@ -10,13 +22,29 @@ module.exports = class Cardiology_Doctor extends Tp.BaseDevice {
     this.name = "Cardiology_Doctor Account for " + this.state.screenName;
     this.description = "This is your Cardiology Doctor Account. You can use it"
       + " to remind patients to track their blood pressure regularly.";
+
+    this._doctor_email = "rickygv@stanford.edu";
+    this._doctor_password = "test";
   }
 
   /*
-   * Get the patient's measurements
+   * Retrieves the patient's blood pressure measurements from the databse
    */
-  get_measurements({ email, password, patient_email }) {
+  get_measurements({ email }) {
     let measurements = null;
-    return [{ measurements: measurements }];
+    options_retrieve.path = '/retrieve?email=' + email;
+
+    https.get(options_retrieve, res => {
+      res.on('data', d => {
+        measurements = d;
+        console.log('Successfully retrieved measurements from database');
+      });
+
+      res.on('end', () => {
+        return [{ measurements: JSON.stringify(measurements) }];
+      })
+    }).on('error', error => {
+      console.error(error);
+    });
   }
 };
--- patienttt/device.js
+++ patienttt/device.js
@@ -1,6 +1,18 @@
 "use strict";
 
 const Tp = require('thingpedia');
+const https = require('https');
+
+const options_upload = {
+  hostname: 'https://almond-cardiology.herokuapp.com',
+  path: '/upload',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Requested-With'
+  }
+};
 
 module.exports = class Cardiology_Patient extends Tp.BaseDevice {
   constructor(engine, state) {
@@ -10,11 +22,35 @@ module.exports = class Cardiology_Patient extends Tp.BaseDevice {
     this.name = "Cardiology_Patient Account for " + this.state.screenName;
     this.description = "This is your Cardiology Patient Account. You can use it"
       + " to upload your blood pressure recordings for your doctor.";
+
+    // TODO: This is test data (will be changed later)
+    this._email = "rickygv@stanford.edu";
+    this._password = "test";
+    this._doctor_email = "rickygv@stanford.edu";
   }
 
   /*
-   * Allows the patient to upload their blood pressure measurements for their doctor
+   * Uploads the patient's blood pressure measurements to the database
    */
   do_record({ measurement }) {
+    let data = JSON.stringify({
+      email: this._email,
+      password: this._password,
+      doctor_email: this._doctor_email,
+      time: new Date(),
+      measurement: measurement
+    });
+
+    let req = https.request(options_upload, res => {
+      console.log('Successfully uploaded measurement to database');
+    });
+
+    req.on('error', error => {
+      console.error(error);
+    })
+
+    req.write(data);
+
+    req.end();
   }
 };
