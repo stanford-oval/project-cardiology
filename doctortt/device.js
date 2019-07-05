7bf2d00d1ede50725e6fa30665b86421bafb7d1d
rickygv99 <rickygv99@yahoo.com>
2019-07-05 11:59:52 -0700

    Updated device.js with placeholder code, Renamed a couple params in manifest.tt

--- doctortt/device.js
+++ doctortt/device.js
@@ -1,107 +1,29 @@
 "use strict";
 
 const Tp = require('thingpedia');
-const twilio = require('twilio'); // https://www.twilio.com
 
-// These values can be found in the Twilio console.
-// WARNING: Need to replace these for production. This is just a test account --
-// no billing information is attached. In production, we will not store the
-// AUTH_TOKEN on GitHub.
-const TWILIO_ACCOUNT_SID = 'ACe43881bb16f358819e5e0150b7d30f2b';
-const TWILIO_AUTH_TOKEN = 'a231bfe257bb39db74731b7e354441da';
-var client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-
-/*
- * Formats the ruleset so that it is readable by humans.
- */
-function format_instructions(times, triggers) {
-  let instructions = null;
-
-  // TODO: Insert code here
-
-  return instructions;
-}
-
-module.exports = class Cardiology extends Tp.BaseDevice {
+module.exports = class Cardiology_Doctor extends Tp.BaseDevice {
   constructor(engine, state) {
     super(engine, state);
 
-    this.uniqueId = "cardiology-" + this.state.userId;
-    this.name = "Cardiology Account for " + this.state.screenName;
-    this.description = "This is your Cardiology Account. You can use it to"
-      + " remind patients to track their blood pressure regularly.";
-
-    this._times = null;
-    this._triggers = null;
+    this.uniqueId = "cardiology_doctor-" + this.state.userId;
+    this.name = "Cardiology_Doctor Account for " + this.state.screenName;
+    this.description = "This is your Cardiology Doctor Account. You can use it"
+      + " to remind patients to track their blood pressure regularly.";
   }
 
   /*
-   * Get alerts about potentially dangerous blood pressure readings
+   * Sign up for a cardiology acccount
    */
-  get_alerts() {
-    let alerts = null;
-
-    // TODO: Insert code here
-
-    return [{ feed: alerts }];
+  do_signup({ email, password }) {
+    // TODO: Signup code here
   }
 
   /*
-   * Create instructions to measure blood pressure
+   * Get the patient's measurements
    */
-  do_create_ruleset({ times, triggers }) {
-    // TODO: Use basic NLP / UI to standardize times and triggers
-
-    // Placeholder code
-    this._times = times;
-    this._triggers = triggers;
-  }
-
-  /*
-   * Get the instructions to measure blood pressure that were most
-   * recently created
-   */
-  get_instructions() {
-    let instructions = format_instructions(this._times, this._triggers);
-    return [{ instructions: instructions }];
-  }
-
-  /*
-   * Update times at which the patient should record their blood pressure
-   */
-  do_update_times({ times }) {
-    // TODO: Use basic NLP / UI to standardize times
-
-    this._times = times;
-  }
-
-  /*
-   * Update blood pressure measurements at which you should be automatically
-   * notified
-   */
-  do_update_triggers({ triggers }) {
-    // TODO: Use basic NLP / UI to standardize triggers
-
-    this._triggers = triggers;
-  }
-
-  /*
-   * Send the blood pressure measurement instructions to the
-   * patient's virtual assistant.
-   * Each patient is uniquely identified by their phone number
-   */
-  do_send_ruleset({ phone_number }) {
-    let instructions = format_instructions(this._times, this._triggers);
-
-    // Sends the SMS message through Twilio
-    client.messages.create({
-      to: phone_number,
-      from: '17149474157', // Twilio trial phone number
-      body: JSON.stringify({
-        times: this._times,
-        triggers: this._triggers,
-        instructions: instructions
-      })
-    });
+  get_measurements({ email, password, patient_email }) {
+    let measurements = null;
+    return [{ measurements: measurements }];
   }
 };
