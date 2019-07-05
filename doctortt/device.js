4610c25a3501fdbf986e019e4d429119b9d08b2e
rickygv99 <rickygv99@yahoo.com>
2019-07-05 16:44:07 -0700

    Fixed bugs in doctor tt

--- doctortt/device.js
+++ doctortt/device.js
@@ -1,23 +1,6 @@
 "use strict";
 
 const Tp = require('thingpedia');
-const https = require('https');
-
-var options_retrieve = {
-  protocol: 'https:',
-  hostname: 'almond-cardiology.herokuapp.com',
-  path: '/retrieve?', // Query parameters are added later during function call
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'X-Requested-With'
-  }
-};
-
-// TODO: This is test data (will be changed later)
-const doctor_email = "rickygv@stanford.edu";
-const doctor_password = "test";
 
 module.exports = class Cardiology_Doctor extends Tp.BaseDevice {
   constructor(engine, state) {
@@ -28,6 +11,7 @@ module.exports = class Cardiology_Doctor extends Tp.BaseDevice {
     this.description = "This is your Cardiology Doctor Account. You can use it"
       + " to remind patients to track their blood pressure regularly.";
 
+    // TODO: This is test data (will be changed later)
     this._doctor_email = "rickygv@stanford.edu";
     this._doctor_password = "test";
   }
@@ -36,15 +20,13 @@ module.exports = class Cardiology_Doctor extends Tp.BaseDevice {
    * Retrieves the patient's blood pressure measurements from the databse
    */
   get_measurements({ email }) {
-    options_retrieve.path = '/retrieve?email=' + email + '&doctor_email=' + doctor_email + '&doctor_password=' + doctor_password;
+    let path = '/retrieve?email=' + email + '&doctor_email=' + this._doctor_email + '&doctor_password=' + this._doctor_password;
 
-    https.get(options_retrieve, res => {
-      res.on('data', d => {
-        console.log('Successfully retrieved measurements from database');
-        return JSON.parse(d.toString());
-      });
-    }).on('error', error => {
-      console.error(error);
+    return Tp.Helpers.Http.get("https://almond-cardiology.herokuapp.com" + path).then((result) => {
+      return JSON.parse(result.toString());
+    }).then((measurements) => {
+      console.log('Successfully retrieved measurements from database');
+      return measurements;
     });
   }
 };
